acf645af0877853c91f32aedc65314a06bf9b25b
JangYeonghun <96059900+JangYeonghun@users.noreply.github.com>
2023-06-06 11:06:57 +0900

    리엑트로 변경 및 인기순위 페이지 구현
    리엑트 변경 및 인기순위 페이지 구현

---
+++ src/Poplink.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Poplink(props) {
+  return(
+    <div className="pop">
+      <ul>인기 순위</ul>
+      <ul className="pop-links"><a href="#">1.</a></ul>
+      <ul className="pop-links"><a href="#">2.</a></ul>
+      <ul className="pop-links"><a href="#">3.</a></ul>
+      <ul className="pop-links"><a href="#">4.</a></ul>
+      <ul className="pop-links"><a href="#">5.</a></ul>
+      <ul className="pop-links"><a href="#">6.</a></ul>
+      <ul className="pop-links"><a href="#">7.</a></ul>
+      <ul className="pop-links"><a href="#">8.</a></ul>
+      <ul className="pop-links"><a href="#">9.</a></ul>
+      <ul className="pop-links"><a href="#">10.</a></ul>
+    </div>
+  );
+}
+
+export default Poplink;
