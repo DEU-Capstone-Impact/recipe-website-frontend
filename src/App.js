1e72ff7251568154dcc0905bfb3655793e2f8b46
JangYeonghun <gnsdudwkd@naver.com>
2023-06-08 03:39:04 +0900

    이전 파일 복구

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import Header from './Header';
+import Home from './Mainpage';
+import RecipeRanking from './Recipe_ranking';
+import RecipeKorea from './Rpkorea';
+import RecipeChina from './Rpchina';
+import RecipeJapan from './Rpjapan';
+import RecipeWestern from './Rpwestern';
+import NotFound from './NotFound';
+import './App.css';
+import DeleteUser from "./deleteUser";
+import LoginPage from "./loginPage";
+import Mypage from "./mypage";
+import MypageUpdate from "./mypageUpdate";
+import RecipeUpdate from "./recipeUpdate";
+import RecipeUpload from "./recipeUpload";
+import RecipeView from "./recipeView";
+import RegisterPage from "./registerPage";
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe-ranking" element={<RecipeRanking />} />
+          <Route path="/Rpkorea" element={<RecipeKorea />} />
+          <Route path="/Rpchina" element={<RecipeChina />} />
+          <Route path="/Rpjapan" element={<RecipeJapan />} />
+          <Route path="/Rpwestern" element={<RecipeWestern />} />
+          <Route path="/deleteUser" element={<DeleteUser />} />
+          <Route path="/loginPage" element={<LoginPage />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypageUpdate" element={<MypageUpdate />} />
+          <Route path="/recipeUpdate" element={<RecipeUpdate />} />
+          <Route path="/recipeUpload" element={<RecipeUpload />} />
+          <Route path="/recipeView" element={<RecipeView />} />
+          <Route path="/registerPage" element={<RegisterPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// djfkl
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,78 @@
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+function Header(props) {
+  const [showCategory, setShowCategory] = useState(false);
+  const [searchValue, setSearchValue] = useState(''); // 검색어 상태값 추가
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    clearTimeout(timeoutRef.current);
+    setShowCategory(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setShowCategory(false);
+    }, 200);
+  };
+
+  const handleMenuClick = () => {
+    setShowCategory(!showCategory);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // 검색어를 이용한 검색 기능 구현 또는 검색 결과 페이지로 이동하는 로직 추가
+    console.log('검색어:', searchValue);
+    setSearchValue('');
+  };
+
+  return (
+    <header>
+      <div className="logo">
+        <Link className='topmenu' to='/'><h3>세상에 나쁜 요리는 없다</h3></Link>
+        <ul className="nav-links">
+          <Link className='topmenu' to="/recipe-ranking">인기순위</Link>
+
+          <li
+            className={`topmenu ${showCategory ? 'active' : ''}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleMenuClick}
+          >
+            레시피
+            {showCategory && (
+              <ul className="category-menu">
+                <li><Link to="/Rpkorea">한식</Link></li>
+                <li><Link to="/Rpchina">중식</Link></li>
+                <li><Link to="/Rpjapan">일식</Link></li>
+                <li><Link to="/Rpwestern">양식</Link></li>
+              </ul>
+            )}
+          </li>
+
+          <Link className='topmenu' to="/recipeUpload">레시피업로드</Link>
+          <form className="search-form" onSubmit={handleSearchSubmit}>
+            <input
+              className="search-input"
+              type="text"
+              placeholder="검색어를 입력하세요"
+              value={searchValue}
+              onChange={handleSearchInputChange}
+            />
+            <button className="search-button" type="submit">검색</button>
+          </form>
+          <Link className='topmenu my-page' to="/mypage">마이페이지</Link>
+          <Link className='topmenu' to="#">로그아웃</Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
